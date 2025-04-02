22855afa965383953be47b6d5215b1da1fba6656
msurve2@uw.edu <msurve2@uw.edu>
2025-04-02 10:11:19 -0700

    Initial Commit

---
+++ HelloiOSTests/HelloiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOSTests.swift
+//  HelloiOSTests
+//
+//  Created by Maansi Surve on 4/2/25.
+//
+
+import Testing
+@testable import HelloiOS
+
+struct HelloiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
